fb925c35da581a57346890428cf7330fc6484779
Jonathas Garcia <jonathas-garcia>
2021-03-11 16:56:17 -0300

    Ajuste teste

--- src/test/java/br/com/jonathas/demojenkinsansible/DemoJenkinsAnsibleApplicationTests.java
+++ src/test/java/br/com/jonathas/demojenkinsansible/DemoJenkinsAnsibleApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.jonathas.demojenkinsansible;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoJenkinsAnsibleApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
